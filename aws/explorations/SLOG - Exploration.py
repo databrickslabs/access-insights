752f08ec4daac2b797ea3ca6a5aac9313e70c49a
samerzabaneh <samerzabaneh>
2025-05-01 18:33:12 +0000

    latest eda changes for good candidates

--- aws/explorations/SLOG - Exploration.py
+++ aws/explorations/SLOG - Exploration.py
@@ -1,17 +1,21 @@
 # Databricks notebook source
-# MAGIC %sql
-# MAGIC select
-# MAGIC   *
-# MAGIC from
-# MAGIC   slog.default.cloudtrail_with_auditevents
-# MAGIC where
-# MAGIC   table_name is not null
-# MAGIC limit 100;
+# MAGIC %md
+# MAGIC ####Look into external tools 
+# MAGIC - Objective - Look into read, write, delete operations on Storage for external tables ONLY.   To identify good candidates that can be migrated to Managed Tables.
+# MAGIC   - Identify if an external table is being leveraged by an external platform. If it is it would not be a good candidate until the write operations subside.
+# MAGIC
+# MAGIC External Table -> catalog.schema.xyz
+# MAGIC - Leveraged by Databricks
+# MAGIC - Leveraged by External Platforms, Read Only for Good Candidate
+# MAGIC
+# MAGIC External Table -> catalog.schema.xyz
+# MAGIC - Leveraged by Databricks
+# MAGIC - Good Candidate would be read, write, delete
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC with good_candidates as (
+# MAGIC WITH good_candidates AS (
 # MAGIC   SELECT
 # MAGIC     table_type,
 # MAGIC     eventCategory,
@@ -21,49 +25,49 @@
 # MAGIC     data_source_format,
 # MAGIC     storage_path,
 # MAGIC     AssumeRole_count,
-# MAGIC     GetObject_count,
-# MAGIC     PutObject_count,
-# MAGIC     DeleteObject_count,
-# MAGIC     ListObjects_count
-# MAGIC   FROM
-# MAGIC     (
-# MAGIC       SELECT
-# MAGIC         table_type,
-# MAGIC         eventCategory,
-# MAGIC         eventSource,
-# MAGIC         requestParameters.prefix AS prefix,
-# MAGIC         table_full_name,
-# MAGIC         table_url,
-# MAGIC         data_source_format,
-# MAGIC         storage_path,
-# MAGIC         eventName,
-# MAGIC         COUNT(*) as event_count
-# MAGIC       FROM
-# MAGIC         slog.default.cloudtrail_with_auditevents
-# MAGIC       WHERE
-# MAGIC         table_name IS NOT NULL
-# MAGIC       GROUP BY
-# MAGIC         table_type,
-# MAGIC         eventCategory,
-# MAGIC         eventSource,
-# MAGIC         requestParameters.prefix,
-# MAGIC         table_full_name,
-# MAGIC         table_url,
-# MAGIC         data_source_format,
-# MAGIC         storage_path,
-# MAGIC         eventName
+# MAGIC     COALESCE(GetObject_count, 0) + COALESCE(ListObjects_count, 0) AS reads,
+# MAGIC     COALESCE(PutObject_count, 0) + COALESCE(DeleteObject_count, 0) AS writes
+# MAGIC   FROM (
+# MAGIC     SELECT
+# MAGIC       table_type,
+# MAGIC       eventCategory,
+# MAGIC       eventSource,
+# MAGIC       requestParameters.prefix AS prefix,
+# MAGIC       table_full_name,
+# MAGIC       table_url,
+# MAGIC       data_source_format,
+# MAGIC       storage_path,
+# MAGIC       eventName,
+# MAGIC       COUNT(*) AS event_count
+# MAGIC     FROM
+# MAGIC       slog.default.cloudtrail_with_auditevents
+# MAGIC     WHERE
+# MAGIC       table_name IS NOT NULL
+# MAGIC     GROUP BY
+# MAGIC       table_type,
+# MAGIC       eventCategory,
+# MAGIC       eventSource,
+# MAGIC       requestParameters.prefix,
+# MAGIC       table_full_name,
+# MAGIC       table_url,
+# MAGIC       data_source_format,
+# MAGIC       storage_path,
+# MAGIC       eventName
+# MAGIC   ) PIVOT (
+# MAGIC     SUM(event_count) FOR eventName IN (
+# MAGIC       'AssumeRole' AS AssumeRole_count,
+# MAGIC       'GetObject' AS GetObject_count,
+# MAGIC       'PutObject' AS PutObject_count,
+# MAGIC       'DeleteObject' AS DeleteObject_count,
+# MAGIC       'ListObjects' AS ListObjects_count
 # MAGIC     )
-# MAGIC       PIVOT (
-# MAGIC         SUM(event_count) FOR eventName IN (
-# MAGIC           'AssumeRole' AS AssumeRole_count,
-# MAGIC           'GetObject' AS GetObject_count,
-# MAGIC           'PutObject' AS PutObject_count,
-# MAGIC           'DeleteObject' AS DeleteObject_count,
-# MAGIC           'ListObjects' AS ListObjects_count
-# MAGIC         )
-# MAGIC       )
+# MAGIC   )
 # MAGIC )
-# MAGIC select
+# MAGIC SELECT
+# MAGIC   case when writes = 0 then 1 else 0 end as `good_candidate`,
 # MAGIC   *
-# MAGIC from
+# MAGIC FROM
 # MAGIC   good_candidates
+# MAGIC WHERE
+# MAGIC   table_type = 'EXTERNAL' 
+# MAGIC   and data_source_format = 'DELTA'
