be0ed60ac98e55b6ef29aabb71f3db173eda3ba0
sjrusso8 <64294847+sjrusso8@users.noreply.github.com>
2025-05-30 11:10:44 -0400

    update pipeline for table size

--- azure/pipelines/utils.py
+++ azure/pipelines/utils.py
@@ -1,14 +1,8 @@
 import requests
 
-from pyspark.sql.types import (
-    StructType,
-    StructField,
-    StringType,
-    LongType,
-    MapType,
-    ArrayType,
-    BooleanType,
-)
+from delta import DeltaTable
+
+from pyspark.sql.types import StructType
 from pyspark.sql import DataFrame, Column, SparkSession
 from pyspark.sql.functions import (
     col,
@@ -16,6 +10,7 @@ from pyspark.sql.functions import (
     explode,
     concat_ws,
     regexp_extract,
+    struct,
 )
 
 
@@ -115,141 +110,49 @@ def azure_apps(
     return spark.createDataFrame(apps_data, schema=schema)
 
 
-hms_table_schema = StructType(
-    [
-        StructField("catalog", StringType(), True),
-        StructField("database", StringType(), True),
-        StructField("name", StringType(), True),
-        StructField("table_format", StringType(), True),
-        StructField("table_type", StringType(), True),
-        StructField("location_uri", StringType(), True),
-        StructField("status", StringType(), True),
-    ]
-)
+def collect_table_details(
+    spark: SparkSession, df: DataFrame, schema: StructType | str
+) -> DataFrame:
+    """Collect all table details"""
 
-azure_apps_schema = StructType(
-    [
-        StructField("id", StringType(), True),
-        StructField("app_id", StringType(), True),
-        StructField("account_enabled", BooleanType(), True),
-        StructField("display_name", StringType(), True),
-        StructField("service_principal_names", ArrayType(StringType(), True), True),
-        StructField("service_principal_type", StringType(), True),
-        StructField("created_datetime", StringType(), True),
-        StructField("deleted_datetime", StringType(), True),
-    ]
-)
+    tables = df.collect()
 
-eventhub_logs_schema = StructType(
-    [
-        StructField(
-            "records",
-            ArrayType(
-                StructType(
-                    [
-                        StructField("time", StringType(), True),
-                        StructField("resourceId", StringType(), True),
-                        StructField("category", StringType(), True),
-                        StructField("operationName", StringType(), True),
-                        StructField("operationVersion", StringType(), True),
-                        StructField("schemaVersion", StringType(), True),
-                        StructField("statusCode", LongType(), True),
-                        StructField("statusText", StringType(), True),
-                        StructField("durationMs", LongType(), True),
-                        StructField("callerIpAddress", StringType(), True),
-                        StructField("correlationId", StringType(), True),
-                        StructField(
-                            "identity",
-                            StructType(
-                                [
-                                    StructField("type", StringType(), True),
-                                    StructField("tokenHash", StringType(), True),
-                                    StructField(
-                                        "authorization",
-                                        ArrayType(
-                                            StructType(
-                                                [
-                                                    StructField(
-                                                        "action", StringType(), True
-                                                    ),
-                                                    StructField(
-                                                        "roleAssignmentId",
-                                                        StringType(),
-                                                        True,
-                                                    ),
-                                                    StructField(
-                                                        "roleDefinitionId",
-                                                        StringType(),
-                                                        True,
-                                                    ),
-                                                    StructField(
-                                                        "principals",
-                                                        ArrayType(
-                                                            StructType(
-                                                                [
-                                                                    StructField(
-                                                                        "id",
-                                                                        StringType(),
-                                                                        True,
-                                                                    ),
-                                                                    StructField(
-                                                                        "type",
-                                                                        StringType(),
-                                                                        True,
-                                                                    ),
-                                                                ]
-                                                            )
-                                                        ),
-                                                        True,
-                                                    ),
-                                                    StructField(
-                                                        "denyAssignmentId",
-                                                        StringType(),
-                                                        True,
-                                                    ),
-                                                    StructField(
-                                                        "type", StringType(), True
-                                                    ),
-                                                    StructField(
-                                                        "result", StringType(), True
-                                                    ),
-                                                    StructField(
-                                                        "reason", StringType(), True
-                                                    ),
-                                                ]
-                                            )
-                                        ),
-                                        True,
-                                    ),
-                                    StructField(
-                                        "requester",
-                                        StructType(
-                                            [
-                                                StructField(
-                                                    "objectId", StringType(), True
-                                                ),
-                                                StructField(
-                                                    "tenantId", StringType(), True
-                                                ),
-                                            ]
-                                        ),
-                                        True,
-                                    ),
-                                ]
-                            ),
-                            True,
-                        ),
-                        StructField("location", StringType(), True),
-                        StructField(
-                            "properties", MapType(StringType(), StringType()), True
-                        ),
-                        StructField("uri", StringType(), True),
-                        StructField("protocol", StringType(), True),
-                        StructField("resourceType", StringType(), True),
-                    ]
-                )
-            ),
-            True,
+    table_details = []
+    for table in tables:
+        table_vals = table.asDict()
+        table_vals["table_details"] = None
+        namespace = (
+            f"`{table.table_catalog}`.`{table.table_schema}`.`{table.table_name}`"
         )
-    ]
-)
+
+        if table.data_source_format == "DELTA" and table.table_type == "EXTERNAL":
+            try:
+                print(namespace)
+                df = (
+                    DeltaTable.forName(spark, namespace)
+                    .detail()
+                    .select(
+                        struct(
+                            col("partitionColumns").alias("partition_cols"),
+                            col("clusteringColumns").alias("clustering_cols"),
+                            col("numFiles").alias("num_files"),
+                            col("sizeInBytes").alias("size_in_bytes"),
+                            col("properties").alias("properties"),
+                            col("minReaderVersion").alias("min_reader_version"),
+                            col("minWriterVersion").alias("min_writer_version"),
+                            col("tableFeatures").alias("table_features"),
+                            col("statistics").alias("statistics"),
+                        ).alias("details")
+                    )
+                )
+
+                table_vals["table_details"] = df.first().details.asDict()
+
+            except Exception as exc:
+                table_vals["status"] = "\n".join(
+                    filter(None, [table_vals.get("status"), str(exc)])
+                )
+
+        table_details.append(table_vals)
+
+    return spark.createDataFrame(table_details, schema=schema)
