b95fc2ff99251634174599500c0642f00b961662
avinkumar-db <avinkumar-db>
2025-07-28 18:00:22 +0000

    New external path access

--- aws/pipelines/part2_cloudtrail_optional(readers)/cloudtrail_systemtable.py
+++ aws/pipelines/part2_cloudtrail_optional(readers)/cloudtrail_systemtable.py
@@ -18,23 +18,6 @@ credentials_schema = StructType([
     StructField("accessKeyId", StringType(), True)
 ])
 
-@dlt.view
-def information_schema():
-    return (spark.read.table("system.information_schema.tables")
-            .filter((col("table_catalog") != "system") & col("storage_path").isNotNull())
-            .select(
-                col("table_catalog"),
-                col("table_schema"),
-                col("table_name"),
-                col("table_type"),
-                col("table_owner"),
-                col("last_altered_by"),
-                col("data_source_format"),
-                col("storage_sub_directory"),
-                col("storage_path"),
-                concat(col("table_catalog"), lit("."), col("table_schema"), lit("."), col("table_name")).alias("full_table_name"),
-                regexp_extract(col("storage_path"), pattern, 1).alias("bucket")
-            ))
 
 @dlt.view
 def cloudtrail_logs_with_storagepath():
